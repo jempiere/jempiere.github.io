d2f81f196d9f264bcc10591c201b585aea370d91
jempiere <65310544+jempiere@users.noreply.github.com>
2021-10-29 19:56:18 +0000

    UI updates, lots of visual reworking

--- js/programs.js
+++ js/programs.js
@@ -1,1 +1,31 @@
 //programs javascript configuration
+
+let toggles = {
+	a: false,
+	b: false,
+	c: false,
+	d: false,
+}
+
+function build(bid = 'button', d1 = 'div', d2 = 'div', toggle = false){
+	let button = dom.idGet(bid);
+	let div1 = dom.idGet(d1);
+	let div2 = dom.idGet(d2);
+
+	button.onclick = e => {
+		toggle = !toggle;
+		if(toggle){
+			div1.classList = '';
+			div2.classList = '';
+		} else {
+			div1.classList = 'hd';
+			div2.classList = 'hd';
+		}
+		log(bid+': '+toggle);
+	}
+}
+
+build('ig','individual', 'group' ,toggles.a);
+build('ce','core', 'enrich', toggles.b);
+build('hp','help', 'prep', toggles.c);
+build('aa','asl', 'art', toggles.d);
