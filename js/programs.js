9cab3e5fc0d356e04316ba013145f944cb6dac5b
jempiere <65310544+jempiere@users.noreply.github.com>
2021-11-20 01:04:19 +0000

    UI updates

--- js/programs.js
+++ js/programs.js
@@ -1,32 +1,21 @@
-//programs javascript configuration
-dom.idGet('programs').classList += ' active';
+/* New Code */
 
-let toggles = {
-	a: false,
-	b: false,
-	c: false,
-	d: false,
+dom.idGet('programs').classList += 'active';
+
+function doListen(){
+	Array.from(dom.clGets('top')).forEach(button => {
+	button.addEventListener('click',()=>{
+		button.classList = 'top active';
+	});
+});
+
+Array.from(dom.clGets('top active')).forEach(button => {
+	button.addEventListener('click',()=>{
+		button.classList = 'top';
+	});
+});
 }
 
-function build(bid = 'button', d1 = 'div', d2 = 'div', toggle = false){
-	let button = dom.idGet(bid);
-	let div1 = dom.idGet(d1);
-	let div2 = dom.idGet(d2);
+doListen();
 
-	button.onclick = e => {
-		toggle = !toggle;
-		if(toggle){
-			div1.classList = '';
-			div2.classList = '';
-		} else {
-			div1.classList = 'hd';
-			div2.classList = 'hd';
-		}
-		log(bid+': '+toggle);
-	}
-}
-
-build('ig','individual', 'group' ,toggles.a);
-build('ce','core', 'enrich', toggles.b);
-build('hp','help', 'prep', toggles.c);
-build('aa','asl', 'art', toggles.d);
+dom.listen('click',doListen);
